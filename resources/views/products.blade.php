8a1222731304873d71a6111b05a4ca9b44f14992
Matiss <matisskrauja31@gmail.com>
2024-07-09 16:47:51 +0300

    added product css file

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,53 +3,129 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="products.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 
         <title>Products</title>
+    </head>
     <body>
-        <table class="table table-hover">
+ 
+        <h2>TOP sales</h2>   
+        <strong><a href="" class="showall">Show all</a></strong>
+            {{-- <div class="container scroll-container">
+                @foreach($items as $item)
+                    <div class="product-block ">
+                         <img src="{{ $item->image_url }}" alt="{{ $item->name }}"> 
+                        <div><img src="images/1.png"></div>
+                        <div class="item-detail">
+                            <div class="image"></div>
+                            
+                        </div>
+                        <div class="item-detail name">
+                            <div class="label ">Name:</div>
+                            <div>{{$item->name}}</div>
+                        </div>
+                        <div class="item-detail">
+                            <div class="label description">Description:</div>
+                            <div>{{$item->description}}</div>
+                        </div> 
+                        
+                        <div class="item-detail">
+                            <div class="label category">Category:</div>
+                            <div>{{$item->category}}</div>
+                        </div>
+                        <div class="item-detail">
+                            <div class="label stock">In Stock:</div>
+                            <div>{{$item->in_stock}}</div>
+                        </div>
+                        <div class="item-detail">
+                            <div class="label size">Size:</div>
+                            <div>{{$item->size}}</div>
+                        </div>
+                        <div class="item-detail">
+                            <div class="label weight">Weight:</div>
+                            <div>{{$item->weight}}</div>
+                        </div> 
+                        <div class="wrap">
+                        <div class="item-detail price">
+                            <div class="label">Price:</div>
+                            <div>{{$item->price}}</div>
+                        </div>
+                        <div class="item-detail discount">
 
-            <thead>
-        
-              <th>name</th>
-        
-              <th>description</th>
+                                <div class="label">Discount:</div>
+                                <div>{{$item->discount}}</div>
+                        </div>
 
-              <th>price</th>
-
-              <th>category</th>
-
-              <th>inStock</th>
-
-              <th>size</th>
-
-              <th>weight</th>
-
-              <th>discount</th>
-
+                        </div>
+                    </div>
+                @endforeach
+            </div> --}}
             
-            </thead>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Horizontal Sliding Gallery</title>
+                <link rel="stylesheet" href="styles.css">
+            
+            
+                <div class="container scroll-container">
+                    @foreach($items as $item)
+                        <div class="product-block">
+                            {{-- <img src="{{ $item->image_url }}" alt="{{ $item->name }}"> --}}
+                            <div><img src="images/1.png" alt="{{ $item->name }}"></div>
+                            <div class="item-detail">
+                                <div class="image"></div>
+                            </div>
+                            
+                            {{-- <div class="item-detail">
+                                <div class="label description">Description:</div>
+                                <div>{{$item->description}}</div>
+                            </div> --}}
+                            {{-- <div class="item-detail">
+                                <div class="label category">Category:</div>
+                                <div>{{$item->category}}</div>
+                            </div>
+                            <div class="item-detail">
+                                <div class="label stock">In Stock:</div>
+                                <div>{{$item->in_stock}}</div>
+                            </div>
+                            <div class="item-detail">
+                                <div class="label size">Size:</div>
+                                <div>{{$item->size}}</div>
+                            </div>
+                            <div class="item-detail">
+                                <div class="label weight">Weight:</div>
+                                <div>{{$item->weight}}</div>
+                            </div> --}}
+                            <div class="item-detail name">
+                                <div class="label">Name:{{$item->name}}</div>
+                            </div>
+                            <div class="wrap">
+                                <div class="item-detail price">
+                                    <div class="label">Price:{{$item->price}}</div>
+                                </div>
+
+                                @if($item->discount !== null)
+                                <div class="item-detail discount">
+                                    <div class="label">Discount:{{$item->discount}}</div>
+                                    
+                                </div>
+                            @endif
+
+                                {{-- <div class="item-detail discount">
+                                    <div class="label">Discount:{{$item->discount}}</div>
+                                </div> --}}
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <script src="scripts.js"></script>
+            </body>
+            </html>
+            
         
-            <tbody>
-        @foreach($items as $item)
-        
-        
-        
-                <tr>
-        
-                  <td>{{$item->name}} </td>
-                  <td>{{$item->description}} </td>
-                  <td>{{$item->price}} </td>
-                  <td>{{$item->category}} </td>
-                  <td>{{$item->in_stock}} </td>
-                  <td>{{$item->size}} </td>
-                  <td>{{$item->weight}} </td>
-                  <td>{{$item->discount}} </td>
-        
-                </tr>
-        @endforeach
-        
-            </tbody>
-        
-        </table>
     </body>
 </html>
