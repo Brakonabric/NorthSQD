0e50434a0999e0268a0431a1d6f146b7be371e2d
Rodions Sokolovs <rodionssokolovs@gmail.com>
2024-07-10 18:24:57 +0300

    Add footer and contact info

---
+++ resources/views/templates/contacts.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/contacts.css') }}">
+</head>
+<body>
+    <div class="container-contacts">
+        <div class="row-contacts">
+            <div class="list">
+                <h4>Contacts</h4>
+                <li class="list__item"><img src="svg/Icon-Address.svg" alt=""><a href="https://www.google.lv/maps/place/Uriekstes+iela+2A,+Zieme%C4%BCu+rajons,+R%C4%ABga,+LV-1005/@56.9895222,24.1091361,15z/data=!3m1!4b1!4m6!3m5!1s0x46eecf7557626a35:0xdcf974b277b9432b!8m2!3d56.9895109!4d24.1194144!16s%2Fg%2F11bw3z9_t5?entry=ttu">Uriekstes iela 2A 1st floor, Riga, LV-1005</a></li>
+                <li class="list__item"><img src="svg/Icon_Phone.svg" alt="">(+371) 25-000-666 </li>
+                <li class="list__item"><img src="svg/Icon_Mail.svg" alt=""> info@northsquad.lv</li>
+            </div>
+        </div>
+        <div class="contact-info">
+            <li class="list__item"><a href="#"><img src="svg/Icon_Twitter.svg" alt=""></a></li>
+            <li class="list__item"><a href="#"><img src="svg/Icon_WhatsApp.svg" alt=""></a></li>
+            <li class="list__item"><a href="#"><img src="svg/Icon_Instagram.svg" alt=""></a></li>
+        </div>
+        <div class="images">
+            <li class="list__item"><img src="svg/master card.svg" alt=""></li>
+            <li class="list__item"><img src="svg/visa.svg" alt=""></li>
+            <li class="list__item"><img src="svg/paypal.svg" alt=""></li>
+            <li class="list__item"><img src="svg/googlePay.svg" alt=""></li>
+        </div>    
+    </div>
+</body>
+</html>
