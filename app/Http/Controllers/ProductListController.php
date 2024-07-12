1b1de008699a54503b1ea280c9088b100037f59f
Deniss Lučkins <ultiden3@gmail.com>
2024-07-12 15:23:42 +0300

    Somewhat added cart mobile page and changed db scheme

--- app/Http/Controllers/ProductListController.php
+++ app/Http/Controllers/ProductListController.php
@@ -9,6 +9,8 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 use App\Models\Item;
+use App\Models\ItemColor;
+use App\Models\ItemSize;
 use DB;
 
 class ProductListController extends Controller
@@ -61,12 +63,24 @@ class ProductListController extends Controller
         if(empty($item)){
             throw new \Exception('Product not found');
         }
-            return view('product',['item'=>$item]);
-        }
+    }
         catch(\Exception $e){
-            
+
             return redirect('/error');
         }
+            $size=null;
+            $color = ItemColor::where('item_id', $id)->first();
+            if($color){
+            $size = ItemSize::where('color_id', $color->id)->first();
+            }
+
+                $items=[
+                    'item'=>$item,
+                    'color'=>$color,
+                    'size'=>$size
+                ];  
+            return view('product',['items'=>$items]);
+        
     }
     public function addProduct(Request $request){
         $request->validate([
@@ -78,17 +92,45 @@ class ProductListController extends Controller
             'weight' => 'required'
         ]);
         
-        Item::create([
+        $item = Item::create([
         'name' => $request->name,
         'description' => $request->description,
         'price' => $request->price,
         'category' => $request->category,
-        'in_stock' => $request->has('in_stock'),
+        #'in_stock' => $request->has('in_stock'),
         'imageUrl' => $request->imageUrl,
-        'size' => $request->size,
         'weight' => $request->weight,
         'discount' => $request->discount
         ]);
+        // foreach($request->colors as $color){
+        //     if(array_key_exists("name",$color)){
+        //     $color = ItemColor::create([
+        //        'in_stock' =>array_key_exists("instock",$color),
+        //        'color' =>$color["name"],
+        //        'item_id' =>$item->id
+        //     ]);
+        //     $sizes=explode(";",$color["sizes"]);
+        //     foreach($sizes as $size){
+        //         $sizeElements=explode(",",$size);
+        //         $instock;
+        //         $sizeEl;
+        //         foreach($sizeElements as $sizeElement){
+        //             if($sizeElement === 1 || $sizeElement === 0){
+        //                 $instock=$sizeElement; 
+        //              }
+        //              else{
+        //                  $sizeEl=$sizeElement;
+        //              }
+        //         }
+        //         ItemSize::create([
+        //            'in_stock' =>$instock,
+        //            'size' =>$sizeEl,
+        //            'color_id' =>$color->id
+        //         ]);
+        //     }
+        // }
+        // }
+        
         return redirect('products');
     }
     public function showAddProductForm()
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,40 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Cart</title>
-    <body>
-        <table class="table table-hover">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
+    <title>Cart</title>
 
-            <thead>
-        
-        
-              <th>name</th>
-              <th>quantity</th>
-              <th>price</th>
-              <th>remove</th>
-        
-            </thead>
-        
-            <tbody>
-        @foreach($cart as $item)
-        
-        
-        
-                <tr>
-        
-                  <td>{{$item['name']}} </td>
-                  <td>{{$item['quantity']}} </td>
-                  <td>{{($item['price'])*($item['quantity'])}} </td>
-                  <td><a href="{{ route('removeFromCart',$item['id']) }}">Remove</a></td>
-        
-                </tr>
+<body>
+    @include('templates/navbar')
+    <h1>Your Cart</h1>
+    <div class="cart_items">
+        @if ($cart)
+        @foreach ($cart as $item)
+            <div class="item_box">
+                <img src="{{ asset('svg/logo.svg') }}" alt="">
+                <div class="item_desc">
+                    <p class="item_title">{{ $item['name'] }}</p>
+                    <p class="item_size">Size</p>
+                    <p class="item_cost">€{{ $item['price'] * $item['quantity'] }}</p>
+                </div>
+                <div class="item_quantity">
+                    <a href="{{ route('removeFromCart', $item['id']) }}"><img onClick="plus()" src="{{ asset('svg/minus.svg') }}" alt=""></a>
+                    <div class="item_number">
+                        <p class = "quantity">{{ $item['quantity'] }}</p>
+                    </div>
+                    <a href="{{ route('addToCart', $item['id']) }}"><img src="{{ asset('svg/plus.svg') }}" alt=""></a>
+                </div>
+            </div>
         @endforeach
-        
-            </tbody>
-        
-        </table>
-    </body>
+        @endif
+        <div class="TotalCost">
+            <p>Subtotal</p>
+            <p>Price</p>
+        </div>
+        <hr>
+    </div>
+</body>
 </html>
