63a85847300ff5fbe4f7b013ed556fd53487dbf8
Katerina <jribajeva@gmail.com>
2024-07-16 16:57:53 +0300

    adding and styling shipping options block

--- resources/views/shippingPolicy.blade.php
+++ resources/views/shippingPolicy.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/shipping.css') }}">
     <title>Shipping Policy</title>
 </head>
 <body>
@@ -12,7 +13,14 @@
     <h1>Shipping Policy</h1>
     <p>At North Squad, we strive to ensure your shopping experience is seamless and convenient. We exclusively use pakomats for all our deliveries, offering you flexibility and reliability.</p>
     <h3>Shipping Options</h3>
-
+    <div class="shipping-options">
+        <div class="shipping-options-item"><div class="shipping-logo"><img src="{{ asset("svg/Omniva.svg") }}" width="60" alt="Omniva" class="logo"></div>
+        <p class="caption-text">Omniva</p></div>
+        <div class="shipping-options-item"><div class="shipping-logo"><img src="{{ asset("svg/DPD.svg") }}" width="42" alt="DPD" class="logo"></div>
+        <p class="caption-text">DPD</p></div> 
+        <div class="shipping-options-item"><div class="shipping-logo"><img src="{{ asset("svg/LatvijasPasts.svg") }}" width="60" alt="Latvijas Pasts" class="logo"></div>
+        <p class="caption-text">Latvijas Pasts</p></div>
+    </div>
     <h3>Processing Time</h3>
     <p>All orders are processed within 1–2 business days. Once your order is  ready, you will receive a notification with tracking information.</p>
     <h3>Delivery Time</h3>
