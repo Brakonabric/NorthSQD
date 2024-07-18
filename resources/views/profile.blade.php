15923ee3106838c6877717ebafbd7885283cae38
Deniss Lučkins <ultiden3@gmail.com>
2024-07-18 10:22:57 +0300

    added Profile page and ability to user data

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+    <title>Cart</title>
+
+<body>
+    @include('templates/navbar')
+    <div class="userBox">
+        <form method=POST action={{route('profilePost')}}>
+        @csrf
+        <div class="userData">
+            <div class="name-surname">
+            <p>Name:</p><p class="notEditThing trickyText"> {{ $user->name }}</p><input class="editThing" name="name" type="text" value={{ $user->name }}>
+            </div>
+            <div class="name-surname">
+            <p>Surname:</p> <p class="notEditThing trickyText"> {{ $user->surname }}</p><input class="editThing" name="surname" type="text" value={{ $user->surname }}>
+            </div>
+            <div class="name-surname">
+            <p>Your e-mail: {{ $user->email }}</p>
+            </div>
+            <div class="name-surname">
+            <p>email: {{ $user->email }}</p>
+            </div>
+        </div>
+        <div class="coolButtons">
+            <button class="notEditThing optionsbut" type="button" onclick="showEdit()">Edit</button>
+            <button class="editThing optionsbut" type="button" onclick="cancel()">Cancel</button>
+            <input class="editThing optionsbut" type="submit" value="Save"></input>
+        </div>
+    </form>
+    </div>
+    @include('templates/footer')
+</body>
+<script>
+    function showEdit() {
+        const editItems=Array.from(document.getElementsByClassName("editThing"));
+        const notEditItems=Array.from(document.getElementsByClassName("notEditThing"));
+        editItems.forEach(item => {
+            item.style.display='inline-block';
+        });
+        notEditItems.forEach(item => {
+            item.style.display='none';
+        });
+
+    }
+    function cancel() {
+        const editItems=Array.from(document.getElementsByClassName("editThing"));
+        const notEditItems=Array.from(document.getElementsByClassName("notEditThing"));
+        editItems.forEach(item => {
+            item.style.display='none';
+        });
+        notEditItems.forEach(item => {
+            item.style.display='inline-block';
+        });
+    }
+
+</script>
+
+</html>
